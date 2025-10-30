5b23d68c5378d4573106e670f76143355e26df58
YociusProductions <90871690+StardustIsArt@users.noreply.github.com>
2025-10-30 13:42:40 -0500

    initializing the 2D array

--- SlotMachine/Program.cs
+++ SlotMachine/Program.cs
@@ -5,5 +5,34 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World! This is a slot machine for you!");
+
+        int[,] grid =  new int[3,3];
+        for (int row = 0; row < 3; row++)
+        {
+            for (int col = 0; col < 3; col++)
+            {
+                grid[row, col] = ' ';
+            }
+        }
+
+        for (int row = 0; row < 3; row++)
+        {
+            for (int col = 0; col < 3; col++)
+            {
+                Console.Write($"{grid[row, col]}");
+                if (col < 3 - 1) Console.Write(" |");
+            }
+            Console.WriteLine();
+            if (row < 3 - 1)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.Write("---");
+                    if (i < 3 - 1) Console.Write("+");
+                }
+                Console.WriteLine();
+            }
+        }
+       
     }
 }
