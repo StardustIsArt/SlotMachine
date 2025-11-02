24c452d0907116fd567a100d8ef6f955776d6de6
YociusProductions <90871690+StardustIsArt@users.noreply.github.com>
2025-11-02 13:32:52 -0600

    added random number for slot entries

--- SlotMachine/Program.cs
+++ SlotMachine/Program.cs
@@ -4,35 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World! This is a slot machine for you!");
+        Console.WriteLine("Step right up and check your luck!");
+        const int SIZE = 3;
+        const int MAX_RANDOM = 100;
+        const int MIN_RANDOM = 0;
+        int[,] grid =  new int[SIZE,SIZE];
+        Random number = new Random();       
+        int randomNumber = number.Next(MIN_RANDOM, MAX_RANDOM);
 
-        int[,] grid =  new int[3,3];
-        for (int row = 0; row < 3; row++)
+        for (int row = 0; row < SIZE; row++)
         {
-            for (int col = 0; col < 3; col++)
+            for (int col = 0; col < SIZE; col++)
             {
                 grid[row, col] = ' ';
             }
         }
 
-        for (int row = 0; row < 3; row++)
+        for (int row = 0; row < SIZE; row++)
         {
-            for (int col = 0; col < 3; col++)
+            for (int col = 0; col < SIZE; col++)
             {
                 Console.Write($"{grid[row, col]}");
-                if (col < 3 - 1) Console.Write(" |");
+                if (col < SIZE - 1) Console.Write(" |");
             }
             Console.WriteLine();
-            if (row < 3 - 1)
+            if (row < SIZE - 1)
             {
-                for (int i = 0; i < 3; i++)
+                for (int i = 0; i < SIZE; i++)
                 {
                     Console.Write("---");
-                    if (i < 3 - 1) Console.Write("+");
+                    if (i < SIZE - 1) Console.Write("+");
                 }
                 Console.WriteLine();
             }
         }
-       
+        
     }
 }
