ef1469ba35f31aea4619ba22bc0c09ca1fa9b3e7
YociusProductions <90871690+StardustIsArt@users.noreply.github.com>
2026-01-20 17:54:37 -0600

    I forget to pass the parameter Random random

--- SlotMachine/GameLogic.cs
+++ SlotMachine/GameLogic.cs
@@ -2,14 +2,14 @@ namespace SlotMachine;
 
 public class GameLogic
 {
-    public static int[,] GenerateReel()
+    public static int[,] GenerateReel(Random random)
     {
         int[,] reel = new int[MachineConstants.REEL_SIZE, MachineConstants.REEL_SIZE];
         for (int row = 0; row < MachineConstants.REEL_SIZE; row++)
         {
             for (int col = 0; col < MachineConstants.REEL_SIZE; col++)
             {
-                reel[row, col] = number.Next(MachineConstants.MIN_RANDOM, MachineConstants.MIN_RANDOM);
+                reel[row, col] = random.Next(MachineConstants.MIN_RANDOM, MachineConstants.MIN_RANDOM);
             }
         }
         return reel;
