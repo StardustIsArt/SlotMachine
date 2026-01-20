0eb0a026360124310f1814a631dec14935acb781
YociusProductions <90871690+StardustIsArt@users.noreply.github.com>
2026-01-20 17:36:59 -0600

    Update Logic.cs

--- SlotMachine/Logic.cs
+++ SlotMachine/Logic.cs
@@ -2,5 +2,15 @@ namespace SlotMachine;
 
 public class GameLogic
 {
-    
+    public static int[,] GenerateReel()
+    {
+        int[,] reel = new int[MachineConstants.REEL_SIZE, MachineConstants.REEL_SIZE];
+        for (int row = 0; row < MachineConstants.REEL_SIZE; row++)
+        {
+            for (int col = 0; col < MachineConstants.REEL_SIZE; col++)
+            {
+                reel[row, col] = number.Next(MachineConstants.MIN_RANDOM, MachineConstants.MIN_RANDOM);
+            }
+        }
+    }
 }
